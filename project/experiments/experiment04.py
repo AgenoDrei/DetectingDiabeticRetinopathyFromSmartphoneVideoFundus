8c947fbc255278c693fc24733ce796fab5c57970
AgenoDrei <s.mueller1995@gmail.com>
2019-10-14 15:16:01 +0200

    Improve lens detection by differentating between big / small lenses

--- project/experiments/experiment04.py
+++ project/experiments/experiment04.py
@@ -1,4 +1,4 @@
-from utils import  load_image, show_image, load_images, show_image_row, print_progress_bar
+from utils import  load_image, show_image, load_images, show_image_row, print_progress_bar, enhance_contrast_image
 import cv2
 import numpy as np
 import sys
@@ -10,7 +10,8 @@ np.set_printoptions(threshold=sys.maxsize)
 
 def run():
     images = load_images(path='./C001R', img_type='png')
-    #images = [enhance_contrast_image(images[i]) for i in range(len(images))]
+    images = [enhance_contrast_image(images[i], clip_limit=4) for i in range(len(images))]
+    show_image_row(images, 'Contrast')
     [detect_circles(img) for img in images]
 
     image = load_image('/home/simon/Videos/Anomaly Dataset/raw_images/SNAP_00035.png')
@@ -22,38 +23,40 @@ def run():
     detect_circles(image)
 
 
-def detect_circles(img:np.array) -> np.array:
+def detect_circles(img:np.array, radius_reduction=20) -> np.array:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.medianBlur(gray, 3)
-    circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img.shape[0] / 3, minRadius=450, maxRadius=600, param1=75, param2=75)
+    img_blur = cv2.medianBlur(gray, 5)
     output = img.copy()
+    circle = None
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            if y < img.shape[0] / 3 or y > img.shape[0] / 3 * 2 or x < img.shape[1] / 3 or x > img.shape[1] / 3 * 2:
-                continue
-            r = 500 if r > 500 else r
-            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            mask = np.zeros((img.shape[0], img.shape[1]), dtype='uint8')
-            cv2.circle(mask, (x, y), r, (255, 255, 255, ), thickness=-1)
-            show_image_row([output, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)], name='circles')
-            return mask
+    #detect small lens
+    small_circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img.shape[0] / 8, minRadius=400, maxRadius=500, param1=90, param2=90)
+    if small_circles is not None:
+        small_circles = np.round(small_circles[0, :]).astype("int")
+        small_circles = [(x, y, r) for (x, y, r) in small_circles if img.shape[0] / 3 < y < img.shape[0] / 3 * 2 and img.shape[1] / 3 < x < img.shape[1] / 3 * 2]
+        circle = sorted(small_circles, key=lambda xyr: xyr[2])[0]
 
+    if circle is None:                   #detect large lens
+        large_circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img.shape[0] / 8, minRadius=500, maxRadius=600, param1=90, param2=90)
+        if large_circles is not None:
+            large_circles = np.round(large_circles[0, :]).astype("int")
+            large_circles = [(x, y, r) for (x, y, r) in large_circles if
+                             img.shape[0] / 3 < y < img.shape[0] / 3 * 2 and img.shape[1] / 3 < x < img.shape[
+                                 1] / 3 * 2]
+            circle = sorted(large_circles, key=lambda xyr: xyr[2])[0]
 
-def enhance_contrast_image(img:np.array):
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=CONTRAST_LIMIT, tileGridSize=(8, 8))
-    cl = clahe.apply(l)
-    #cl = cv2.equalizeHist(l)
-    ca = clahe.apply(a)
-    cb = clahe.apply(b)
+    if circle is not None:
+        (x, y, r) = circle
+        r -= radius_reduction
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        mask = np.zeros((img.shape[0], img.shape[1]), dtype='uint8')
+        cv2.circle(mask, (x, y), r, (255, 255, 255,), thickness=-1)
+        show_image_row([output, cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)], name='circles')
+        return mask
 
-    limg = cv2.merge((cl, a, b))
-    final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-    return final
+    print('INFO> No mask found')
+    return None
 
 
 '''
